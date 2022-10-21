e530a2ad96675b395d8d3afc70e1f99ffbc31532
yk-st <the.oc@hotmail.co.jp>
2022-10-21 16:10:46 +0900

    commit

--- web/apps/app.py
+++ web/apps/app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail
-from flask_login import LoginManager
 
 import importlib
 import logging
