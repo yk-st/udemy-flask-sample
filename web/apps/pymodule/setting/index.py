0041868f8554791a90b025a4ab73a01b29455d85
yk-st <the.oc@hotmail.co.jp>
2022-10-25 15:52:13 +0900

    commit

--- web/apps/pymodule/setting/index.py
+++ web/apps/pymodule/setting/index.py
@@ -24,7 +24,7 @@ def money():
 
                 money:MONEY = MONEY(
                         # 認証認可が入ると、この処理はリクエストヘッダーから取得することができる
-                        system_id = "111111"
+                        system_id = 111111
                 )
 
                 money_record = money.get_data()
@@ -34,7 +34,7 @@ def money():
                         # データがある場合とない場合で分けることができる
                         money_record = MONEY(
                                 # 認証認可が入ると、この処理はリクエストヘッダーから取得することができる
-                                system_id = "111111",
+                                system_id = 111111,
                                 soushisan =  form.soushisan.data
                         )
                 else:
