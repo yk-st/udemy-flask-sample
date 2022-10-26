ca66a48805c85f32b0700a6d5bc65b0716ba8516
yk-st <the.oc@hotmail.co.jp>
2022-10-26 21:08:35 +0900

    commit

--- web/apps/app.py
+++ web/apps/app.py
@@ -93,6 +93,9 @@ def create_app():
     from apps.pymodule.inqury import index as inqury_view
     app.register_blueprint(inqury_view.inqury)
 
+    from apps.pymodule.mislenious import index as mislenious_view
+    app.register_blueprint(mislenious_view.mislenious)
+
     return app
 
 @app.errorhandler(500)
