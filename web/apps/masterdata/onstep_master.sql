69622c19a1cc737d65b4a45dba7506be98a9538c
yk-st <the.oc@hotmail.co.jp>
2022-10-27 21:43:57 +0900

    commit

---
+++ web/apps/masterdata/onstep_master.sql
@@ -0,0 +1,4 @@
+use hoge;
+insert into hoge.onestep_master (onestep_id, description, checked) values(1, "ほげ", False);
+insert into hoge.onestep_master (onestep_id, description, checked) values(2, "ぺけ", False);
+insert into hoge.onestep_master (onestep_id, description, checked) values(3, "ぽん", False);
